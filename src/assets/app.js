5a9e0d60e94bb65c688bac29f6d8ebd38f565041
Abdrashitov Timur <timurlan156@gmail.com>
2023-09-18 21:40:35 +0300

    init

--- src/assets/app.js
+++ src/assets/app.js
@@ -1,19 +1,28 @@
+const INTERVAL_DELAY = 1000;
+
 let markInterval = 0;
 let currentMarkedWordIndex = 0;
 let paragraphElement = document.getElementById('task-block__text');
+let paragraphStr = paragraphElement.innerText;
 
 function markTextWords() {
   clearInterval(markInterval);
   
-  let paragraphStr = paragraphElement.innerText;
+  markNextTextWord(paragraphStr)
   
   markInterval = setInterval(function () {
-    markNextTextWord(paragraphStr)
-  }, 100);
+    markNextTextWord()
+  }, INTERVAL_DELAY);
 }
 
-function markNextTextWord(paragraphStr) {
+function markNextTextWord() {
   let paragraphWordsArr = paragraphStr.split(' ');
+  
+  if (!paragraphWordsArr[currentMarkedWordIndex].match(/[\wа-я]+/ig)) {
+    currentMarkedWordIndex++;
+    markNextTextWord();
+    return;
+  }
   
   paragraphWordsArr[currentMarkedWordIndex] = `
     <span class="marked-word">${paragraphWordsArr[currentMarkedWordIndex]}</span>
